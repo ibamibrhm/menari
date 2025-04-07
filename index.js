2076b3181db483c43f87b9b81141b3a9ac8ce224
Ibrahim <ibrahimibam29@gmail.com>
2025-04-07 16:45:06 +0700

    raw passthrough the output

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 #!/usr/bin/env node
 
 const { spawn } = require('child_process');
-const ls = spawn('bash', [ '-c', 'curl -s -L https://raw.githubusercontent.com/ibamibrhm/menari/master/roll.sh | bash' ]);
-
-ls.stdout.setEncoding('utf8');
-
-ls.stdout.on('data', (data) => {
-  console.log(data.replace(/.*m/, ''))
-});
-
-ls.stderr.on('data', (data) => {
-  console.log(data.toString());
-});
-
-ls.on('close', (code) => {
-  console.log(`exit with code ${code}`);
+const ls = spawn('bash', ['-c', 'curl -s -L https://raw.githubusercontent.com/ibamibrhm/menari/master/roll.sh | bash'], {
+  stdio: 'inherit'
 });
